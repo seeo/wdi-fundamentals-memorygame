001cbb16815332be87c8f8a6e66e24082d4df2ff
seeo <aragonlotr5@gmail.com>
2019-03-09 20:03:18 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,7 @@ var cards = [
   {
     rank: "queen",
     suit: "hearts",
-    cardImage: 	"images/queen-of-hearts.png"
+    cardImage: "images/queen-of-hearts.png"
   },
   {
     rank: "queen",
@@ -20,6 +20,7 @@ var cards = [
     cardImage: "images/king-of-diamonds.png"
   },
 ];
+
 var cardsInPlay = [];
 var checkForMatch = function(){
   if(cardsInPlay[0] === cardsInPlay[1]){
@@ -29,14 +30,34 @@ var checkForMatch = function(){
     alert("Sorry, try again.");
     }
 }
-var flipCard = function(cardId){
-    console.log("User flipped "+cards[cardId].rank);
-    console.log("User flipped "+cards[cardId].suit);
-    console.log("User flipped "+cards[cardId].cardImage);
+var flipCard = function(){
+    var cardId = this.getAttribute('data-id');
+    console.log("User flipped " + cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].suit);
+    console.log("User flipped " + cards[cardId].cardImage);
     cardsInPlay.push(cards[cardId].rank);
+    this.setAttribute('src', cards[cardId].cardImage)
     if(cardsInPlay.length === 2){
       checkForMatch();
       };
 };
-flipCard(0);
-flipCard(2);
+
+//flipCard(0);
+//flipCard(2);
+
+var createBoard = function(){
+   //const cardTable = document.getElementById('game-board');
+  for (var i = 0; i < cards.length; i++){
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+    // cardTable.appendChild(cardElement);
+    // Q: how does the above function know that the cards are suposed to be placed within the <main> tag of the HTML?
+    // Ans: It is because of the 'game-board' Id that is nested between the <main> tags.
+    // Q: Why does appendChild(), not use inverted commas for items inside the ()?
+  };
+};
+
+createBoard();
